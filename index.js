9099fefa112257dc1d00342a75dae48f14b250b2
Ridwan <aladebaruwa@gmail.com>
2023-06-08 18:54:05 +0100

    JS

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function myFunction() {
-  let text = document.getElementById("demo").innerHTML; 
-  document.getElementById("demo").innerHTML =
-  text.replace("Microsoft","W3Schools");
-}
-let texts = "I love cats. Cats are very easy to love. Cats are very popular."
+let texts = "I love cats. Cats are very easy to love. Cats are very popular.";
+let textz = "I love cats. Cats are very easy to love. Cats are very popular.";
 texts = texts.replaceAll("Cats","Dogs");
-texts = texts.replaceAll("cats","dogs");
-document.write(texts)
+textz = textz.replaceAll(/Cats/g,"dogs");
+alert(texts);
+alert(textz);
+
+function myFunction() {
+  let text = document.getElementById("demo").innerHTML;
+  document.getElementById("demo").innerHTML =
+  text.toUpperCase();
+}
