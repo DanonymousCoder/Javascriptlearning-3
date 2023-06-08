a98b7ca6bb8013f940e71a0764f76cd4b4041c9d
Ridwan <aladebaruwa@gmail.com>
2023-06-08 16:11:09 +0100

    add

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let text = "Please visit Microsoft!";
-function myFunction(){
-    let text = document.getElementById("demo") .innerHTML;
-    document.getElementById("demo") .innerHTML =
-     text.replace(/MICROSOFT/i, "W3Schools");
-}
+function myFunction() {
+    let text = document.getElementById("demo").innerHTML; 
+    document.getElementById("demo").innerHTML =
+    text.replace("MICROSOFT","W3Schools");
+  }
