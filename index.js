9d3440e9592fcd67e1a99a7f5bdf04e49fe47361
Ridwan <aladebaruwa@gmail.com>
2023-06-08 18:39:12 +0100

    add

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function myFunction() {
-    let text = document.getElementById("demo").innerHTML; 
-    document.getElementById("demo").innerHTML =
-    text.replace("MICROSOFT","W3Schools");
-  }
+  let text = document.getElementById("demo").innerHTML; 
+  document.getElementById("demo").innerHTML =
+  text.replace("Microsoft","W3Schools");
+}
+let texts = "I love cats. Cats are very easy to love. Cats are very popular."
+texts = texts.replaceAll("Cats","Dogs");
+texts = texts.replaceAll("cats","dogs");
+document.write(texts)
