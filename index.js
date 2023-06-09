0ce3064c6c518d862ab7964968bd429fcc67e276
Ridwan <aladebaruwa@gmail.com>
2023-06-10 00:03:17 +0100

    JS

--- index.js
+++ index.js
@@ -77,3 +77,11 @@ console.log(typeof strPrim2); // "string"
 console.log(typeof strPrim3); // "string"
 console.log(typeof strObj); // "object"
 
+const s1 = "2 + 2"; // creates a string primitive
+const s2 = new String("2 + 2"); // creates a String object
+console.log(eval(s1)); // returns the number 4
+console.log(eval(s2)); // returns the string "2 + 2"
+console.log(eval(s2.valueOf())); // returns the number 4
+console.log(eval(s2.valueOf())); // returns the number 4
+
+
