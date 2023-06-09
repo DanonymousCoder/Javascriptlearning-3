d1b588b4a8bf4d8169534ac0228771de044a27c8
Ridwan <aladebaruwa@gmail.com>
2023-06-10 00:01:28 +0100

    JS

--- index.js
+++ index.js
@@ -67,4 +67,13 @@ if (a < b) {
 function areEqualCaseInsensitive(str1, str2) {
   return str1.toUpperCase() === str2.toUpperCase();
 }
+const strPrim = "foo"; // A literal is a string primitive
+const strPrim2 = String(1); // Coerced into the string primitive "1"
+const strPrim3 = String(true); // Coerced into the string primitive "true"
+const strObj = new String(strPrim); // String with new returns a string wrapper object.
+
+console.log(typeof strPrim); // "string"
+console.log(typeof strPrim2); // "string"
+console.log(typeof strPrim3); // "string"
+console.log(typeof strObj); // "object"
 
